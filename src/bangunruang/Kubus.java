c5d6a2627130b2186b01a4961be0a8cf6d23a4d9
DerickCc <derickchainatra@gmail.com>
2022-07-13 02:00:19 +0700

    Create Kubus.java

---
+++ src/bangunruang/Kubus.java
@@ -0,0 +1,15 @@
+package bangunruang;
+
+public class Kubus {
+    public int  = 0;
+    public float volume = 0;
+
+    public void hitungVolumeBola(){
+        volume = 4 * PI * r * r * r / 3;
+    }
+
+    public void tampilkanVolumeBola(){
+        hitungVolumeBola();
+        System.out.println("Volume Bola (Jari-jari: "+ r +" cm) = "+ volume +" cm3\n");
+    }
+}
